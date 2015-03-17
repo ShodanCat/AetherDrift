07c8de211860057a83480adbc252240322bfeda8
ShodanCat <molleh@gmail.com>
2015-03-17 17:04:04 -0500

    terminals and clicking on stuff

---
+++ Assets/Scripts/SelectObject.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectObject : MonoBehaviour
+{
+	void OnMouseDown()
+	{
+		Debug.Log ("Selected this object!");
+	}
+}
---
+++ Assets/Scripts/StartTerminal.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartTerminal : MonoBehaviour
+{
+	void OnMouseDown()
+	{
+		Debug.Log ("Reading this terminal!");
+		GUI.Label(new Rect(5,5,400,100), "This is a terminal that would give useful info or story.");
+	}
+}
